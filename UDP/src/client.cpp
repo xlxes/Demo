4889d710f63d755944c717cb16b3cd6fcf00cde9
x <xlxes>
2020-02-04 21:32:09 +0800

    add udp demo

--- UDP/src/client.cpp
+++ UDP/src/client.cpp
@@ -6,47 +6,87 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <iostream>
 #include <unistd.h>
-#define MAXLINE 4096
 
-//输入服务器的IP地址
-const char IP_server[] = "192.168.124.2"; //设置服务器地址 即本机地址
+using namespace std;
+
+const int MAX_LEN = 4096;                //缓存区长度
+const char serverip[] = "192.168.124.2"; //设置服务器地址 此处为本机地址
+const int servercom = 8080;              //设置服务器监听端口
+const int localcom = 8081;               //设置本机端口
 
 int main()
 {
-    int sockfd, n;
-    char recvline[4096], sendline[4096];
-    struct sockaddr_in servaddr;
+    int fd;                                    //通信描述符
+    int count;                                 //接收字节数
+    char recvbuff[MAX_LEN], sendbuff[MAX_LEN]; //定义接收缓存区和发送缓存区
+    struct sockaddr_in localaddr;
+    struct sockaddr_in serveraddr;
 
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    socklen_t locallen = sizeof(localaddr);
+    socklen_t serverlen = sizeof(serveraddr);
+
+    //建立UDP的通信文件描述符
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
-        printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
-        return 0;
+        cout << "creat fd error" << endl;
+        return -1;
     }
 
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(6666);
-
-    if (inet_pton(AF_INET, IP_server, &servaddr.sin_addr) <= 0)
+    //填写服务器地址信息
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(servercom); //此处转换为网络字节序
+    //填入服务器的IP并转换为网络字节序
+    if (inet_pton(AF_INET, serverip, &serveraddr.sin_addr) <= 0)
     {
-        printf("inet_pton error for %s\n", IP_server);
-        return 0;
+        cout << "transf  serverip error!" << endl;
+        return -1;
     }
 
-    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    //设定本客户端的地址信息
+    memset(&localaddr, 0, sizeof(localaddr));
+    localaddr.sin_family = AF_INET;
+    localaddr.sin_port = htons(localcom);          //此处转换为网络字节序
+    localaddr.sin_addr.s_addr = htonl(INADDR_ANY); //设置本地IP
+
+    //绑定通信描述符和本地地址端口
+    if ((bind(fd, (struct sockaddr *)&localaddr, locallen)) < 0)
     {
-        printf("connect error: %s(errno: %d)\n", strerror(errno), errno);
-        return 0;
+        cout << "bind fail" << endl;
+        return -1;
     }
 
-    printf("send msg to server: \n");
-    fgets(sendline, 4096, stdin);
-    if (send(sockfd, sendline, strlen(sendline), 0) < 0)
+    cout << "client init successed" << endl;
+    cout << "port:" << ntohs(localaddr.sin_port)
+         << "  ip:" << inet_ntoa(localaddr.sin_addr) << endl;
+
+    while (1)
     {
-        printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
-        return 0;
+        cout << "input:" << endl;
+        memset(sendbuff, 0, sizeof(sendbuff)); //清零
+        cin.getline(sendbuff, MAX_LEN);        //等待写入
+        //向服务器发送数据 从localaddr发到serveraddr
+        if (sendto(fd, sendbuff, sizeof(sendbuff), 0, (struct sockaddr *)&serveraddr, serverlen) < 0)
+        {
+            cout << "send fail!" << endl;
+            return -1;
+        }
+        //从服务器接受消息
+        count = recvfrom(fd, recvbuff, sizeof(recvbuff), 0, (struct sockaddr *)&serveraddr, &serverlen);
+        if (count == -1)
+        {
+            cout << "receive fail" << endl;
+            return -1;
+        }
+        else
+            cout << "receive message: " << recvbuff << endl; //显示接收的字符
+        cout << "clien port:" << ntohs(localaddr.sin_port)
+             << "  ip:" << inet_ntoa(localaddr.sin_addr) << endl;
+        cout << "server port:" << ntohs(serveraddr.sin_port)
+             << "  ip:" << inet_ntoa(serveraddr.sin_addr) << endl;
     }
-    close(sockfd);
+    close(fd);
     return 0;
 }
--- UDP/src/server.cpp
+++ UDP/src/server.cpp
@@ -1,50 +1,73 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <iostream>
 
-#define MAXLINE 4096
+using namespace std;
 
-int main(int argc, char** argv){
-    int  listenfd, connfd;
-    struct sockaddr_in  servaddr;
-    char  buff[4096];
-    int  n;
+const int MAX_LEN = 4096;   //缓存区长度
+const int servercom = 8080; //服务器端口
 
-    if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
-        printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
-        return 0;
+int main()
+{
+    int listenfd;
+    struct sockaddr_in servaddr; //服务器监听信息
+    struct sockaddr_in clieaddr; //获取客户端地址信息
+    socklen_t serverlen = sizeof(servaddr);
+    socklen_t clienlen = sizeof(clieaddr);
+
+    char buff[MAX_LEN];
+    int recv_count = 0;
+
+    //建立监听文件描述符
+    if ((listenfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+    {
+        cout << "creat fd error!" << endl;
+        return -1;
     }
 
+    //填入服务器通信地址信息
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(6666);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //地址为任意本机地址
+    servaddr.sin_port = htons(servercom);
 
-    if( bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
-        printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);
-        return 0;
+    //将地址信息与描述符绑定
+    if (bind(listenfd, (struct sockaddr *)&servaddr, serverlen) < 0)
+    {
+        cout << "bind error!" << endl;
+        return -1;
     }
 
-    if( listen(listenfd, 10) == -1){
-        printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);
-        return 0;
-    }
-
-    printf("======waiting for client's request======\n");
-    while(1){
-        if( (connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1){
-            printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
-            continue;
+    //必要信息输出
+    cout << "server init successed!" << endl;
+    cout << "port:" << ntohs(servaddr.sin_port)
+         << "  ip:" << inet_ntoa(servaddr.sin_addr) << endl;
+    cout << "wait for connect!" << endl;
+    while (1)
+    {
+        //等待从clieaddr地址接收数据，监听数据端口为servaddr，同时把客户端信息放在clieaddr中
+        recv_count = recvfrom(listenfd, buff, MAX_LEN, 0, (struct sockaddr *)&clieaddr, &clienlen);
+        if (recv_count == -1)
+        {
+            cout << "receive data error!" << endl;
+            return -1;
         }
-        n = recv(connfd, buff, MAXLINE, 0);
-        buff[n] = '\0';
-        printf("recv msg from client: %s\n", buff);
-        close(connfd);
+        buff[recv_count] = '\0';                          //在收到的数据后添加结束符号
+        cout << "recv msg from client: " << buff << endl; //输出接收的字符
+        //将原数据发给客户端clieaddr
+        sendto(listenfd, buff, sizeof(buff), 0, (struct sockaddr *)&clieaddr, clienlen);
+        //显示客户端的地址和服务端的地址
+        cout << "clien port:" << ntohs(clieaddr.sin_port)
+             << "  ip:" << inet_ntoa(clieaddr.sin_addr) << endl;
+        cout << "server port:" << ntohs(servaddr.sin_port)
+             << "  ip:" << inet_ntoa(servaddr.sin_addr) << endl;
     }
     close(listenfd);
     return 0;
